c1c91ef3fe694f3716956c65a32357e350d1d41b
kijada <kacperjada@gmail.com>
2024-07-04 21:50:51 +0200

    Update synteza_wody_sem.c

--- zaliczenie/synteza_wody_sem.c
+++ zaliczenie/synteza_wody_sem.c
@@ -16,8 +16,8 @@
 
 // -------------------------------------------------------------------------------- //
 
-#define ITERATIONS_COUNT            10 // water molecules to generate
-#define PRODUCERS_COUNT             5 
+#define MOLECULES_TO_CREATE         10
+#define PRODUCERS_COUNT             8
 
 #define SHERED_MEMORY_NAME          "/kk_mem_water"
 
@@ -26,38 +26,52 @@
 
 // -------------------------------------------------------------------------------- //
 
-void sleep_rand() {
-    int sleep_time = 100 + rand() % (1000 - 100);
+void random_sleep() {
+    int sleep_time = 1000 + rand() % 2000;
     printf("Sleeping for %d ms\n", sleep_time);
     usleep(sleep_time * 1000);
 }
 
+uint8_t count_water_molecules(uint8_t h, uint8_t o) {
+    uint8_t water_molecules_from_hydrogen = h / 2;
+    uint8_t water_molecules_from_oxygen = o;
+    
+    if (water_molecules_from_hydrogen < water_molecules_from_oxygen) {
+        return water_molecules_from_hydrogen;
+    } else {
+        return water_molecules_from_oxygen;
+    }
+}
+
 // -------------------------------------------------------------------------------- //
 
-void water_generator(char *producers, uint8_t *runing) {
+void water_generator(char *producers, uint8_t *running) {
     int i, j;
-    int *address;
     sem_t *sem_gen, **sem_prod;
     char sem_name[50];
     char elements_buffer[PRODUCERS_COUNT + 1];
     int safety_counter = 100;
-    uint8_t condition = 0;
+    uint8_t thread_running;
 
-    // init generator semaphore
-    sem_gen = sem_open(SEMAPHORE_NAME_GENERATOR, O_CREAT, 0600, 1);
+    uint8_t generated_water_molecules = 0;
+    uint8_t posible_water_molecules;
+    uint8_t hydrogen_count, oxygen_count;
+    
+    // Init generator semaphore
+    sem_gen = sem_open(SEMAPHORE_NAME_GENERATOR, O_CREAT, 0600, 0);
     if(sem_gen == SEM_FAILED) {
         perror("Problem with opening semaphore in water generator");
         exit(1);
     }
 
-    // init producer semaphores
+    // Init producer semaphores
     sem_prod = malloc(sizeof(sem_t *) * PRODUCERS_COUNT);
     if (sem_prod == NULL) {
         perror("Failed to allocate memory for sem_prod");
         exit(1);
     }
     for (i = 0; i < PRODUCERS_COUNT; i++) {
-        sprintf(sem_name, "%s_%d", SEMAPHORE_NAME_PRODUCE, i);
+        snprintf(sem_name, sizeof(sem_name), "%s_%d", SEMAPHORE_NAME_PRODUCE, i);
         sem_prod[i] = sem_open(sem_name, O_CREAT, 0600, 1);
         if (sem_prod[i] == SEM_FAILED) {
             perror("Failed to open semaphore in water generator");
@@ -67,99 +81,162 @@ void water_generator(char *producers, uint8_t *runing) {
 
     printf("Start water generator (PID: %d) (func: %s)\n", getpid(), __func__);
 
-    while(safety_counter && *runing) {
+    while(safety_counter && *running && generated_water_molecules < MOLECULES_TO_CREATE) {
         memcpy(elements_buffer, producers, PRODUCERS_COUNT);
         elements_buffer[PRODUCERS_COUNT] = '\0';
-        printf("Water molecule: %s\n", elements_buffer);
-
-        condition = 1;
+        printf("\nWater molecule: %s\n", elements_buffer);
+        thread_running = 0;
+        
+        // Check if there are enough elements to create water molecule
+        hydrogen_count = 0;
+        oxygen_count = 0;
         for (j = 0; j < PRODUCERS_COUNT; j++) {
-            if (producers[j] != 'H') {
-                condition = 0;
-                printf("Waiting for producer no. %d\n", j);
-                producers[j] = '-';
-                sem_post(sem_prod[j]);
-                sem_wait(sem_gen);
+            if (producers[j] == 'H') {
+                hydrogen_count++;
+            } else if (producers[j] == 'O') {
+                oxygen_count++;
             }
         }
 
-        if (condition == 1) {
-            printf("Water molecule created\n");
-            break;
+        // Create water molecule if there are enough elements
+        posible_water_molecules = count_water_molecules(hydrogen_count, oxygen_count);
+        if (posible_water_molecules > 0) {
+            generated_water_molecules += posible_water_molecules;
+            hydrogen_count = posible_water_molecules * 2;
+            oxygen_count = posible_water_molecules;
+
+            printf("# Water molecule created (%d) (new: %d)\n", generated_water_molecules, posible_water_molecules);
+
+            // Remove elements from producers
+            for (j = 0; j < PRODUCERS_COUNT; j++) {
+                if (producers[j] == 'H') {
+                    printf("!\tClearing producer no. %d (H)\n", j);
+                    producers[j] = '-';
+                    hydrogen_count--;
+                    sem_post(sem_prod[j]);
+                    thread_running++;
+                    if (hydrogen_count == 0) {
+                        break;
+                    }
+                }
+            }
+            for (j = 0; j < PRODUCERS_COUNT; j++) {
+                if (producers[j] == 'O') {
+                    printf("!\tClearing producer no. %d (O)\n", j);
+                    producers[j] = '-';
+                    oxygen_count--;
+                    sem_post(sem_prod[j]);
+                    thread_running++;
+                    if (oxygen_count == 0) {
+                        break;
+                    }
+                }
+            }
+
+            // Clear random elements
+            for (j = 0; j < PRODUCERS_COUNT/3; j++) {
+                i = rand() % PRODUCERS_COUNT;
+                if (producers[i] != '-') {
+                    printf("!!\tClearing producer no. %d (random)\n", i);
+                    producers[i] = '-';
+                    sem_post(sem_prod[i]);
+                    thread_running++;
+                }
+            }
+        }
+        else {
+            printf("# Not enough elements to make water!\n");
+
+            // Clear elements
+            for (j = 0; j < PRODUCERS_COUNT; j++) {
+                printf("!!\tClearing producer no. %d\n", j);
+                producers[j] = '-';
+                sem_post(sem_prod[j]);
+                thread_running++;
+            }
         }
 
+        for (j = 0; j < thread_running; j++) {
+            sem_wait(sem_gen);
+        }
+
+        printf(">\tIteration end\n");
         safety_counter--;
     }
+
+    printf("Safety counter: %d\n", safety_counter);
+    printf("Generated water molecules: %d\n", generated_water_molecules);
 
     memcpy(elements_buffer, producers, PRODUCERS_COUNT);
     elements_buffer[PRODUCERS_COUNT] = '\0';
     printf("Water molecule: %s\n", elements_buffer);
 
-    // signal producers to end
-    *runing = 0;
-    for (int j = 0; j < PRODUCERS_COUNT; j++) {
+    // Signal producers to end
+    *running = 0;
+    for (j = 0; j < PRODUCERS_COUNT; j++) {
         sem_post(sem_prod[j]);
     }
 
+    for (i = 0; i < PRODUCERS_COUNT; i++) {
+        sem_close(sem_prod[i]);
+        snprintf(sem_name, sizeof(sem_name), "%s_%d", SEMAPHORE_NAME_PRODUCE, i);
+        sem_unlink(sem_name);
+    }
+
+    sem_close(sem_gen);
+    sem_unlink(SEMAPHORE_NAME_GENERATOR);
     free(sem_prod);
+
     printf("><\tEnd of water generator\n");
 }
 
 // -------------------------------------------------------------------------------- //
 
-void elements_producer(char *producers, uint8_t producer_id, uint8_t *runing) {
-    int i = 0, r;
-    int *address;
-    sem_t *sem_gen, **sem_prod;
+void elements_producer(char *container, uint8_t producer_id, uint8_t *running) {
+    int r;
+    sem_t *sem_gen, *sem_prod;
     char sem_name[50];
-    char elements_buffer[PRODUCERS_COUNT + 1];
+
     srand(time(NULL) + producer_id);
 
-    // init generator semaphore
+    // Init generator semaphore
     sem_gen = sem_open(SEMAPHORE_NAME_GENERATOR, O_CREAT, 0600, 0);
-    if(sem_gen == SEM_FAILED) {
-        perror("Problem with opening semaphore in water generator");
+
+    // Init producer semaphores
+    snprintf(sem_name, sizeof(sem_name), "%s_%d", SEMAPHORE_NAME_PRODUCE, producer_id);
+    sem_prod = sem_open(sem_name, O_CREAT, 0600, 0);
+
+    if (sem_prod == SEM_FAILED || sem_gen == SEM_FAILED) {
+        perror("Failed to open semaphore in water generator");
         exit(1);
     }
 
-    // init producer semaphores
-    sem_prod = malloc(sizeof(sem_t *) * PRODUCERS_COUNT);
-    if (sem_prod == NULL) {
-        perror("Failed to allocate memory for sem_prod");
-        exit(1);
-    }
-    for (i = 0; i < PRODUCERS_COUNT; i++) {
-        sprintf(sem_name, "%s_%d", SEMAPHORE_NAME_PRODUCE, i);
-        sem_prod[i] = sem_open(sem_name, O_CREAT, 0600, 1);
-        if (sem_prod[i] == SEM_FAILED) {
-            perror("Failed to open semaphore in water generator");
-            exit(1);
-        }
-    }
+    printf("Start producer no. %d (PID: %d) (func: %s)\n", producer_id, getpid(), __func__);
 
-    printf("Start prfucer no. %d (PID: %d) (func: %s)\n", producer_id, getpid(), __func__);
-
-    sem_wait(sem_prod[producer_id]);
-
-    while(*runing) {
-        sem_wait(sem_prod[producer_id]);
-        if(!*runing) {
+    while(*running) {
+        printf("~\t\t\t\t\t\tWaiting for generator %d\n", producer_id);
+        sem_wait(sem_prod);
+        if(!*running) {
             break;
         }
 
         r = rand() % 2;
         if(r == 0) {
-            producers[producer_id] = 'H';
+            *container = 'H';
         } else {
-            producers[producer_id] = 'O';
+            *container = 'O';
         }
-        printf("\t\t\tProducer no. %d: %c\n", producer_id, producers[producer_id]);
+        printf(">\t\t\t\t\t\tProducer no. %d: %c\n", producer_id, *container);
 
-        sleep_rand();
+        random_sleep();
         sem_post(sem_gen);
     }
 
-    free(sem_prod);
+    sem_close(sem_gen);
+    sem_unlink(SEMAPHORE_NAME_GENERATOR);
+    sem_close(sem_prod);
+    sem_unlink(sem_name);
+
     printf("<>\tEnd of producer no. %d\n", producer_id);
 }
 
@@ -175,13 +252,12 @@ int main() {
     int i;
 
     srand(time(NULL));
-
+    printf("----------------------------------------------------------------------------------------------------\n");
     printf("Starting water synthesis (%s)\n", __FILE__);
 
     // Clean up after previous run
     shm_unlink(SHERED_MEMORY_NAME);
     sem_unlink(SEMAPHORE_NAME_GENERATOR);
-    sem_unlink(SEMAPHORE_NAME_PRODUCE);
 
     // init shared memory
     printf("Init shared memory\n");
@@ -207,7 +283,7 @@ int main() {
     for (i = 0; i < PRODUCERS_COUNT; i++) {
         pid = fork();
         if(pid == 0) {
-            elements_producer(producers_list, i, runing);
+            elements_producer(&producers_list[i], i, runing);
             return 0;
         }
         process_pid_list[i] = pid;
@@ -229,7 +305,8 @@ int main() {
     munmap(shared_memory_printer, PRODUCERS_COUNT*sizeof(char));
     shm_unlink(SHERED_MEMORY_NAME);
     sem_unlink(SEMAPHORE_NAME_GENERATOR);
-    sem_unlink(SEMAPHORE_NAME_PRODUCE);
+
+    printf("--------------------------------------------------------------------------------\n");
 
     return 0;
 }
