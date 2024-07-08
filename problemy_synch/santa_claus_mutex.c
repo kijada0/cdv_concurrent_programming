5a608cb01335d729edd5bf0f1caaa097e0e43fc9
Kacper Ka <188354e@gmail.com>
2024-07-08 13:58:59 +0200

    clean up and added status monitor

--- problemy_synch/santa_claus_mutex.c
+++ problemy_synch/santa_claus_mutex.c
@@ -10,16 +10,16 @@
 
 // -------------------------------------------------------------------------------- //
 
-#define RENIFER_COUNT       5
-#define RENIFER_TRESHOLD    3
+#define RENIFER_COUNT       9
+#define RENIFER_TRESHOLD    9
 
-#define ELF_COUNT           5
+#define ELF_COUNT           10
 #define ELF_TRESHOLD        3
 
-#define ITERATIONS_COUNT    5
-#define SANTA_WAIT_LIMIT    25
+#define ITERATIONS_COUNT    10
 
-#define NEED_HELP_PROB      5
+#define NEED_HELP_PROB_R    5
+#define NEED_HELP_PROB_E    8
 
 // -------------------------------------------------------------------------------- //
 
@@ -30,6 +30,8 @@ typedef struct {
     void (*task_func)(uint8_t, char *);
     uint8_t *runing;
     uint8_t *waiting_queue;
+    uint8_t *state;
+    uint8_t help_probability;
     pthread_mutex_t *mutex;
     pthread_mutex_t *mutex_bussy;
     pthread_mutex_t *mutex_bussy_santa;
@@ -44,13 +46,21 @@ typedef struct {
     uint8_t *elf_waiting;
     uint8_t *renifer_waiting;
     pthread_mutex_t *mutex;
-    pthread_mutex_t *mutex_cond;
     pthread_mutex_t *mutex_bussy_santa;
     pthread_cond_t *condition_renifer;
     pthread_cond_t *condition_elf;
     pthread_cond_t *condition_santa;
     pthread_cond_t *condition_finnish;
 } santa_data_t;
+
+typedef struct {
+    uint8_t *runing;
+    uint8_t *renifer_waiting;
+    uint8_t *renifer_state_tab;
+    uint8_t *elf_waiting;
+    uint8_t *elf_state_tab;
+    pthread_mutex_t *mutex;
+} santa_wife_data_t;
 
 // -------------------------------------------------------------------------------- //
 
@@ -155,6 +165,7 @@ void *worker_process(void *args) {
 
     memcpy(&params, (thread_data_t *)args, sizeof(thread_data_t));
     mutex = params.mutex;
+    *params.state = 0;
     pthread_mutex_unlock(mutex);
 
     sleep(1);
@@ -167,16 +178,18 @@ void *worker_process(void *args) {
         pthread_mutex_lock(mutex);
         printf("%s%s (%d) runing: %d\n", params.message_prefix, params.name, params.id, *params.runing);
         if(*params.runing == 0) {
+            *params.state = 9;
             pthread_mutex_unlock(mutex);
             break;
         }
         pthread_mutex_unlock(mutex);
 
-        r = rand() % NEED_HELP_PROB;
+        r = rand() % params.help_probability;
         if(r == 0) {
             printf("%s%s (%d): Waiting for Santa\n", params.message_prefix, params.name, params.id);
             pthread_mutex_lock(mutex);
             *params.waiting_queue += 1;
+            *params.state = 1;
             pthread_mutex_unlock(mutex);
             
             barrier_wait(params.barrier);
@@ -185,9 +198,11 @@ void *worker_process(void *args) {
             // Check if Santa is runing
             pthread_mutex_lock(mutex);
             if(*params.runing == 0) {
+                *params.state = 9;
                 pthread_mutex_unlock(mutex);
                 break;
             }
+            *params.state = 2;
             pthread_mutex_unlock(mutex);
             
             pthread_mutex_lock(params.mutex_bussy);
@@ -199,6 +214,7 @@ void *worker_process(void *args) {
 
             // Run task
             pthread_mutex_lock(mutex);
+            *params.state = 3;
             task = params.task_func;
             task(params.id, params.message_prefix);
             pthread_mutex_unlock(mutex);
@@ -209,6 +225,7 @@ void *worker_process(void *args) {
 
             pthread_mutex_lock(params.mutex);
             *params.waiting_queue -= 1;
+            *params.state = 0;
             printf("%s%s (%d): End Runing with Santa\n", params.message_prefix, params.name, params.id);
             pthread_mutex_unlock(params.mutex);
 
@@ -237,15 +254,94 @@ void elf_task(uint8_t id, char *prefix) {
 
 // -------------------------------------------------------------------------------- //
 
+void *santa_wife_process(void *args) {
+    santa_wife_data_t *params = (santa_wife_data_t *)args;
+    uint8_t i;
+    char elf_status_str[ELF_COUNT + 1];
+    char renifer_status_str[RENIFER_COUNT + 1];
+
+    printf("Santa's wife: Start\n");
+
+    while(1) {
+        pthread_mutex_lock(params->mutex);
+        if(*params->runing == 0) {
+            pthread_mutex_unlock(params->mutex);
+            break;
+        }
+        pthread_mutex_unlock(params->mutex);
+
+        pthread_mutex_lock(params->mutex);
+
+
+        for(i = 0; i < RENIFER_COUNT; i++) {
+            switch(params->renifer_state_tab[i]) {
+                case 0:
+                    renifer_status_str[i] = '~';
+                    break;
+                case 1:
+                    renifer_status_str[i] = 'W';
+                    break;
+                case 2:
+                    renifer_status_str[i] = 'B';
+                    break;
+                case 3:
+                    renifer_status_str[i] = 'R';
+                    break;
+                case 9:
+                    renifer_status_str[i] = 'X';
+                    break;
+            }
+        }
+        renifer_status_str[RENIFER_COUNT] = '\0';
+
+        for(i = 0; i < ELF_COUNT; i++) {
+            switch(params->elf_state_tab[i]) {
+                case 0:
+                    elf_status_str[i] = '~';
+                    break;
+                case 1:
+                    elf_status_str[i] = 'W';
+                    break;
+                case 2:
+                    elf_status_str[i] = 'B';
+                    break;
+                case 3:
+                    elf_status_str[i] = 'R';
+                    break;
+                case 9:
+                    elf_status_str[i] = 'X';
+                    break;
+            }
+        }
+        elf_status_str[ELF_COUNT] = '\0';
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[WIFE] \t renifer: waiting: %d | %s \t elf: waiting: %d | %s\n", *params->renifer_waiting, renifer_status_str, *params->elf_waiting, elf_status_str);
+        
+        pthread_mutex_unlock(params->mutex);
+
+        sleep(1);
+    }
+
+    pthread_mutex_lock(params->mutex);
+    *params->runing = 0;
+    pthread_mutex_unlock(params->mutex);
+
+    printf("Santa's wife: End\n");
+}
+
+
+// -------------------------------------------------------------------------------- //
+
 int main() {
     int i;
     uint8_t runing = 1;
     uint8_t elf_waiting = 0, renifer_waiting = 0;
+    uint8_t elf_state[ELF_COUNT] = {0}, renifer_state[RENIFER_COUNT] = {0};
 
     santa_data_t santa_args;
     thread_data_t thread_args;
     pthread_t thread_tab_renifer[RENIFER_COUNT];
     pthread_t thread_tab_elf[ELF_COUNT];
+    pthread_t thread_santa_wife;
 
     pthread_mutex_t mutex;
     pthread_mutex_t mutex_cond;
@@ -259,6 +355,7 @@ int main() {
 
     barrier_t barrier_renifer;
     barrier_t barrier_elf;
+    santa_wife_data_t santa_wife_args;
 
     // ---------------------------------------- //
 
@@ -272,11 +369,6 @@ int main() {
     // init mutex
     printf("Init mutex\n");
     if(pthread_mutex_init(&mutex, NULL) != 0) {
-        perror("pthread_mutex_init");
-        exit(1);
-    }
-
-    if(pthread_mutex_init(&mutex_cond, NULL) != 0) {
         perror("pthread_mutex_init");
         exit(1);
     }
@@ -352,6 +444,15 @@ int main() {
     santa_args.condition_santa = &condition_santa;
     santa_args.condition_finnish = &condition_finnish;
 
+    // init santa wife args
+    santa_wife_args.runing = &runing;
+    santa_wife_args.elf_waiting = &elf_waiting;
+    santa_wife_args.renifer_waiting = &renifer_waiting;
+    santa_wife_args.elf_state_tab = elf_state;
+    santa_wife_args.renifer_state_tab = renifer_state;
+
+    santa_wife_args.mutex = &mutex;
+
     // ---------------------------------------- //
 
     // Run renifers
@@ -359,6 +460,7 @@ int main() {
     strcpy(thread_args.name, "Renifer");
     strcpy(thread_args.message_prefix, "\t\t\t\t\t");
     thread_args.waiting_queue = &renifer_waiting;
+    thread_args.help_probability = NEED_HELP_PROB_R;
     thread_args.task_func = &renifer_task;
     thread_args.barrier = &barrier_renifer;
     thread_args.condition_client = &condition_renifer;
@@ -367,11 +469,13 @@ int main() {
         pthread_mutex_lock(&mutex);
         printf("Spawn Renifer (%d)\n", i);
         thread_args.id = i;
+        thread_args.state = &renifer_state[i];
 
         if(pthread_create(&thread_tab_renifer[i], NULL, worker_process, &thread_args) != 0) {
             perror("pthread_create");
             exit(1);
         }
+        usleep(1000);
     }
 
     // Run elfs
@@ -379,6 +483,7 @@ int main() {
     strcpy(thread_args.name, "Elf");
     strcpy(thread_args.message_prefix, "\t\t\t\t\t\t\t\t\t\t");
     thread_args.waiting_queue = &elf_waiting;
+    thread_args.help_probability = NEED_HELP_PROB_E;
     thread_args.task_func = &elf_task;
     thread_args.barrier = &barrier_elf;
     thread_args.condition_client = &condition_elf;
@@ -387,11 +492,22 @@ int main() {
         pthread_mutex_lock(&mutex);
         printf("Spawn Elf (%d)\n", i);
         thread_args.id = i;
+        thread_args.state = &elf_state[i];
 
         if(pthread_create(&thread_tab_elf[i], NULL, worker_process, &thread_args) != 0) {
             perror("pthread_create");
             exit(1);
         }
+        usleep(1000);
+    }
+
+    // ---------------------------------------- //
+
+    // Run Santa's wife
+    printf("Spawning Santa's wife...\n");
+    if(pthread_create(&thread_santa_wife, NULL, santa_wife_process, &santa_wife_args) != 0) {
+        perror("pthread_create");
+        exit(1);
     }
 
     // ---------------------------------------- //
@@ -413,10 +529,13 @@ int main() {
         pthread_join(thread_tab_renifer[i], NULL);
         printf("\tRenifer (%d) joined\n", i);
     }
+
     for (i = 0; i < ELF_COUNT; i++) {
         pthread_join(thread_tab_elf[i], NULL);
         printf("\tElf (%d) joined\n", i);
     }
+
+    pthread_join(thread_santa_wife, NULL);
 
     // ---------------------------------------- //
 
